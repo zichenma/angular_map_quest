7816a34ff5ecb4b0c2468b2a6b2375497f602874
Zichen Ma <ZMa@firsthelp.com>
2017-05-25 17:18:42 -0400

    not filter yet, 5/25

--- javascript/api.js
+++ javascript/api.js
@@ -1,26 +1,52 @@
 //this file contains all apis data
 
 var apis = {
-    "Call Vol Over Time" : "http://apis.firsthelpfinancial.com/ords/collectionreports/call_vol_overtime_by_colector/",
+    "Current Active Repo Assignments": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/current_active_repo_asgnmnt/",
 
-    "Current Repo Status Summary" : "http://apis.firsthelpfinancial.com/ords/collectionreports/current_repo_status_summary/",
+    "Current Repo Status Summary": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/current_repo_status_summary/",
 
-    "Current Active Repo Assignments" : "http://apis.firsthelpfinancial.com/ords/collectionreports/current_active_repo_asgnmnt/",
+    "Charge off List": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/charge_offs_list/",
 
-    "Funding GPS" : "http://apis.firsthelpfinancial.com/ords/collectionreports/coll_funding_gps/",
+    "Change Offs By State Overtime":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/chrge_offs_by_state_ovrtme/",
+
+    "Call Counts By Week":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/coll_call_counts_by_wk/",
+
+    "Complaint Status Changes":
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/complaint_status_changes/",
+
+    "Call Vol Over Time": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/call_vol_over_time",
+
+    "Call Vol Over Time By Collector": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/call_vol_overtime_by_colector",
+
+    "Funding GPS": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/funding_gps_report/",
     
-    "Liquidation Report" : "http://apis.firsthelpfinancial.com/ords/collectionreports/liquidation/",
+    "Liquidation Report": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/liquidation_report/",
 
-    "Missing Titles Age Thres hold" : "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles_age_threshold/",
+    "Misssing Titles Reports": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles_report/",
+
+    "Missing Titles Age Thres hold": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles_age_threshold/",
     
-    "Misssing Titles Reports" : "http://apis.firsthelpfinancial.com/ords/collectionreports/missing_titles/",
-    
-     "Open Complaints": "http://apis.firsthelpfinancial.com/ords/collectionreports/open_complaints/",
+    "Open Complaints": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/open_complaints/",
      
-     "Repo Vendor Status": "http://apis.firsthelpfinancial.com/ords/collectionreports/repo_vendor_status/",
-     
-     "Charge off List": "http://apis.firsthelpfinancial.com/ords/collectionreports/charge_offs_list/",
+    "Repo Vendor Status": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/repo_vendor_status/",
+
+    // "Repo Status Summary": 
+    // "http://apis.firsthelpfinancial.com/ords/collectionreports/currnt_repo_status_summry/",
     
-     "Suggested Collections Actions List": "http://apis.firsthelpfinancial.com/ords/collectionreports/suggested_collections_actions/"
+    "Suggested Collections Actions List": 
+    "http://apis.firsthelpfinancial.com/ords/collectionreports/suggested_collections_actions/"
 }
 
